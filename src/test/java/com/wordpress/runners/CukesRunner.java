24461f2a551e0e58e5b5329397333f5650030b78
auzundere <admin@192.168.1.2>
2018-03-26 23:59:51 -0500

    wordpress initial commit by Abdullah @11:59 pm on 3/26/2018

---
+++ src/test/java/com/wordpress/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.wordpress.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		plugin= {
+				"html:target/cucumber_report"		
+		},
+		tags="@Dev",
+		features = "src/test/resources/com/wordpress/features/", 
+		glue = "com/wordpress/step_defs",
+		dryRun=false
+)
+public class CukesRunner extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/test/java/com/wordpress/step_defs/Hooks.java
@@ -0,0 +1,38 @@
+package com.wordpress.step_defs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.wordpress.utilities.ConfigurationReader;
+import com.wordpress.utilities.Driver;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hooks {
+	@Before
+	public void beforeScenario(Scenario scenario) {
+		WebDriver driver = Driver.getDriver();
+		driver.get(ConfigurationReader.getProperty("url"));
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		//System.out.println("Before the Scenario started");
+	}
+
+	@After
+	public void afterScenario(Scenario scenario) {
+		//System.out.println("After the Scenario ended");
+		//taking a screenshot
+		if(scenario.isFailed()) {
+			final byte[] screenshot = ((TakesScreenshot) 
+			Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+			//adding the screenshot to the report
+			scenario.embed(screenshot, "image/png");
+		}
+		Driver.closeDriver();
+	}
+}
