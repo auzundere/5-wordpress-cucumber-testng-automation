d23bac135015c643563cf200e24b92181b5cbc21
fatimesimsek <fatimasimsek@gmail.com>
2018-04-01 08:58:29 -0400

    edit_profile.feature file added by Fatime

--- src/test/java/com/wordpress/pages/CommentsPage.java
+++ src/test/java/com/wordpress/pages/CommentsPage.java
@@ -23,6 +23,7 @@ public class CommentsPage {
 		String actualURL = driver.getCurrentUrl();
 		String expectedURL= "http://34.223.219.142:1022/wordpress/wp-admin/edit-comments.php";
 		assertEquals(actualURL,expectedURL,actualURL + " was not the same with the expected URL of " +expectedURL );
+		
 	}
 	
 }
