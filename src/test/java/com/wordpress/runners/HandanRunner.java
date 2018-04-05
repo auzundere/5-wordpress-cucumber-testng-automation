1e9fd1164faaf7dc0678e3289c2e70e02a5ad9b2
auzundere <admin@192.168.1.2>
2018-04-05 10:42:30 -0500

    Added PersonalRunner Files by Abdullah

---
+++ src/test/java/com/wordpress/runners/HandanRunner.java
@@ -0,0 +1,18 @@
+package com.wordpress.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		plugin= {
+				"html:target/cucumber_report",
+				"json:target/cucumber.json"		
+				},
+		tags="@Posts",
+		features = "src/test/resources/com/wordpress/features/", 
+		glue = "com/wordpress/step_defs",
+		dryRun=false
+)
+public class HandanRunner extends AbstractTestNGCucumberTests {
+
+}
